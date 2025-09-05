4a2ead2b62d91908c1e750573a9992f52b7aaf80
abhinayhasti <abhinayhasti46@gmail.com>
2025-09-05 17:13:46 -0400

    updated the format

--- src/main.tf
+++ src/main.tf
@@ -2,11 +2,11 @@ terraform {
   # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
   # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
   #backend "s3" {
-    #bucket         = "cc-tf-state-backend-ci-cd"
-    #key            = "tf-infra/terraform.tfstate"
-    #region         = "us-east-1"
-    #dynamodb_table = "terraform-state-locking"
-    #encrypt        = true
+  #bucket         = "cc-tf-state-backend-ci-cd"
+  #key            = "tf-infra/terraform.tfstate"
+  #region         = "us-east-1"
+  #dynamodb_table = "terraform-state-locking"
+  #encrypt        = true
   #}
 
   required_version = ">=0.13.0"
